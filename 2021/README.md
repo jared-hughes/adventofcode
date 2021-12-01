04b4ed74d58b9b4164e49e386630a9d2da0baa6c
Jared Hughes <jahughes241@gmail.com>
2021-11-30 21:09:13 -0800

    2021 day 1

---
+++ 2021/README.md
@@ -0,0 +1,10 @@
+## 2020
+
+JavaScript solutions are run with node `v16.7.0` via tye integrated terminal of Visual Studio Code.
+For contests I did in real-time, I may have edited them slightly afterward, but no major revisions.
+
+Notes on contests I did:
+
+| Contest Number | End Time 1 | Rank 1 | End Time 2 | Rank 2 | Notes                                     | Learned                                                                                                                        |
+| -------------- | ---------: | :----- | ---------: | :----- | ----------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------ |
+| 1              |      02:47 | 1242   |      04:25 | 462    | Messed up with off-by-one error in part 1 | Don't mess with `arr.slice().map((e,i) => ...)`. Just avoid using sliced indices and sliced elements elements at the same time |
---
+++ 2021/day01b.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+// All imported in advance because typing speed
+const {
+  sort,
+  int,
+  print,
+  clog,
+  sum,
+  product,
+  chr,
+  ord,
+  isAllEqual,
+  isIncreasing,
+  isStrictlyIncreasing,
+  isDecreasing,
+  isStrictlyDecreasing,
+  increments,
+  allDistinct,
+} = require("../utils/utils.js");
+require("lodash.combinations");
+require("lodash.multicombinations");
+require("lodash.permutations");
+require("lodash.multipermutations");
+require("lodash.product");
+const _ = require("lodash");
+const math = require("mathjs");
+
+fs.promises.readFile("day01.in", { encoding: "utf8" }).then((dataString) => {
+  // Remove the last character (the closing newline)
+  dataString = dataString.slice(0, dataString.length - 1);
+  const L = dataString.split(/\n/g);
+  solve(L.map((line) => int(line)));
+});
+
+function solve(M) {
+  L = M.slice(2).map((e, i) => M[i] + M[i + 1] + M[i + 2]);
+  print(sum(L.slice(1).map((e, i) => e > L[i])));
+}
