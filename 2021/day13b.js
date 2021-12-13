6ef30acc24218cf6f6aca475dc96dc9807e69979
Jared Hughes <jahughes241@gmail.com>
2021-12-12 21:22:49 -0800

    Day 13

---
+++ 2021/day13b.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+// All imported in advance because typing speed
+const {
+  sort,
+  int,
+  print,
+  clog,
+  sum,
+  product,
+  chr,
+  ord,
+  isAllEqual,
+  isIncreasing,
+  isStrictlyIncreasing,
+  isDecreasing,
+  isStrictlyDecreasing,
+  increments,
+  allDistinct,
+} = require("../utils/utils.js");
+require("lodash.combinations");
+require("lodash.multicombinations");
+require("lodash.permutations");
+require("lodash.multipermutations");
+require("lodash.product");
+const _ = require("lodash");
+const math = require("mathjs");
+
+fs.promises.readFile("day13.in", { encoding: "utf8" }).then((dataString) => {
+  // Remove the last character (the closing newline)
+  dataString = dataString.slice(0, dataString.length - 1);
+  const L = dataString;
+  solve(L.split(/\n\n/g));
+});
+
+function solve([dots, folds]) {
+  dots = dots.split(/\n/g).map((line) => line.split(/,/).map(int));
+  folds = folds
+    .split(/\n/g)
+    .map((line) => line.split(/=/))
+    .map(([a, b]) => [[...a].at(-1), int(b)]);
+  w = _.max(dots.map(([x, y]) => x)) + 1;
+  h = _.max(dots.map(([x, y]) => y)) + 1;
+  let grid = [];
+  for (y = 0; y < h; y++) {
+    grid.push([]);
+    for (x = 0; x < w; x++) {
+      grid.at(-1).push(0);
+    }
+  }
+  for ([x, y] of dots) {
+    grid[y][x] = 1;
+  }
+  for ([xy, p] of folds) {
+    if (xy == "y") grid = transpose(grid);
+    for (let y = 0; y < h; y++) {
+      for (let x = p; x < grid[0].length; x++) {
+        if (grid[y][x] > 0) {
+          grid[y][x] = 0;
+          grid[y][2 * p - x] = 1;
+        }
+      }
+    }
+    if (xy == "y") grid = transpose(grid);
+  }
+  grid.slice(0, 10).map((line) =>
+    print(
+      line
+        .slice(0, 100)
+        .map((e) => (e ? "#" : " "))
+        .join(``)
+    )
+  );
+}
+
+function transpose(matrix) {
+  return matrix[0].map((col, i) => matrix.map((row) => row[i]));
+}
