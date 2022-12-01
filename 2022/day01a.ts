7fa50def97aa395294decfaf3d378e5ec6f7529e
Jared Hughes <jahughes241@gmail.com>
2022-11-30 20:52:28 -0800

    Ready the imports, man the cannons!

--- 2022/day01a.ts
+++ 2022/day01a.ts
@@ -1,9 +1,15 @@
 import { readFileSync } from "fs";
-import { int, overlappingSlices } from "./utils";
+// prettier-ignore
+import {
+  sort, sortByKey, max, min, int, clog, print, sum, prod, chr, ord, isAllEqual,
+  isIncreasing, isStrictlyDecreasing, isStrictlyIncreasing, overlappingSlices,
+  range, transpose, uniq, dropAtIndex, permutations, cartesianPower,
+  cartesianProduct
+} from "./utils";
 
 let dataString = readFileSync("day01.in", { encoding: "utf-8" })
   // Remove the last character (the closing newline)
   .slice(0, -1);
 let L = dataString.split(/\n/g).map(int);
 
-console.log(L);
+clog(L);
--- 2022/utils.ts
+++ 2022/utils.ts
@@ -1,4 +1,10 @@
 export const sort = (arr: number[]) => arr.sort((a, b) => a - b);
+export const sortByKey = <T>(a: T[], key: (v: T) => number) =>
+  a.sort((u, v) => key(u) - key(v));
+export const max = (arr: number[]) =>
+  arr.reduce((a, b) => Math.max(a, b), -Infinity);
+export const min = (arr: number[]) =>
+  arr.reduce((a, b) => Math.min(a, b), Infinity);
 export const int = (s: string) => parseInt(s, 10);
 export const clog = console.log;
 // single-argument print, returning given value
@@ -19,3 +25,36 @@ export const isStrictlyDecreasing = (a: number[]) =>
   a.every((e, i) => i == 0 || e < a[i - 1]);
 export const overlappingSlices = (a: number[], n: number) =>
   a.slice(n - 1).map((_, i) => a.slice(i, i + n));
+export const range = (n: number) => [...Array(n).keys()];
+export const transpose = <T>(a: T[][]) =>
+  range(Math.min(...a.map((row) => row.length))).map((i) =>
+    a.map((row) => row[i])
+  );
+export const uniq = <T>(a: T[]) => [...new Set(a)];
+export const dropAtIndex = <T>(a: T[], i: number) =>
+  a.slice(0, i).concat(a.slice(i + 1));
+/** Permutations of `s` of length `n` */
+export function permutations<T>(a: T[], n: number = a.length) {
+  if (n > a.length)
+    throw new Error(
+      `Cannot take a permutation of length ${n} from a list of length ${a.length}`
+    );
+  function _permutations(b: T[], prefix: T[]) {
+    if (prefix.length === n) return [prefix];
+    return b.flatMap((e, i) =>
+      _permutations(dropAtIndex(b, i), prefix.concat([e]))
+    );
+  }
+  return _permutations(a, []);
+}
+/** Cartesian product of lists */
+// TODO: tuple-of-arrays type
+export function cartesianProduct<T>(...a: T[][]): T[][] {
+  if (a.length === 0) return [[]];
+  return cartesianProduct(...a.slice(1)).flatMap((p) =>
+    a[0].map((e) => [e].concat(p))
+  );
+}
+export function cartesianPower<T>(a: T[], n: number) {
+  return cartesianProduct(...range(n).map((_) => a));
+}
