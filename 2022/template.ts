edf4bbb9fe6c9d7e1d4925bea785e4589f093e0e
Jared Hughes <jahughes241@gmail.com>
2022-12-02 21:26:26 -0800

    Add template

---
+++ 2022/template.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+// prettier-ignore
+import {
+  sort, sortByKey, max, min, int, clog, print, sum, prod, chr, ord, isAllEqual,
+  isIncreasing, isStrictlyDecreasing, isStrictlyIncreasing, overlappingSlices, slices,
+  range, transpose, uniq, dropAtIndex, permutations, cartesianPower,
+  cartesianProduct, set, intersect, setdiff, symdiff, union
+} from "./utils";
+
+let dataString = readFileSync("day00.in", { encoding: "utf-8" })
+  // Remove the last character (the closing newline)
+  .slice(0, -1);
+
+clog(dataString.split(/\n/g));
