2d1c7f2ead84678cded588f69aeb6af35084e855
Jared Hughes <jahughes241@gmail.com>
2022-12-06 02:53:32 -0800

    Remove duplicated list spread on Day 6

--- 2022/day06.ts
+++ 2022/day06.ts
@@ -3,7 +3,7 @@ import { getInput, clog, isAllDistinct, overlappingSlices } from "./utils";
 let chars = [...getInput("day06.in")];
 
 // part 1
-clog(overlappingSlices([...chars], 4).findIndex(isAllDistinct) + 4);
+clog(overlappingSlices(chars, 4).findIndex(isAllDistinct) + 4);
 
 // part 2
-clog(overlappingSlices([...chars], 14).findIndex(isAllDistinct) + 14);
+clog(overlappingSlices(chars, 14).findIndex(isAllDistinct) + 14);
