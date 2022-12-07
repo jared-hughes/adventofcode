ff06b4361ae21272d13243118f4a8450a84c8dfc
Jared Hughes <jahughes241@gmail.com>
2022-12-06 21:22:48 -0800

    Day 7

---
+++ 2022/day07.ts
@@ -0,0 +1,36 @@
+import { getInput, clog, int, min, sum, range } from "./utils";
+
+let dataString = getInput("day07.in");
+
+let pos: string[] = [];
+
+let sizes = new Map();
+
+for (let r of dataString.split(/\$/g).slice(1)) {
+  r = r.slice(1, -1);
+  if (r.startsWith("cd")) {
+    const dir = r.slice(3);
+    if (dir == "/") pos = [];
+    else if (dir == "..") pos.pop();
+    else pos.push(dir);
+  } else {
+    // ls
+    for (let line of r.slice(3).split(/\n/g)) {
+      if (!line.startsWith("dir")) {
+        range(pos.length + 1).forEach((i) => {
+          let pwd = "/" + pos.slice(0, i).join("/");
+          sizes.set(pwd, (sizes.get(pwd) ?? 0) + int(line));
+        });
+      }
+    }
+  }
+}
+
+const vals = [...sizes.values()];
+
+// Part 1
+clog(sum(vals.filter((e) => e <= 100000)));
+
+// Part 2
+const todelete = sizes.get("/") - 40000000;
+clog(min(vals.filter((e) => e >= todelete)));
