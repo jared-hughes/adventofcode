a1a82e97b3915e55a025ec5a1427290732e514e5
Jared Hughes <jahughes241@gmail.com>
2022-12-20 21:48:49 -0800

    Day 21 no cheese (no eval, no mathematica)

--- 2022/day21.ts
+++ 2022/day21.ts
@@ -2,42 +2,56 @@ import { getInput, clog, int } from "./utils";
 
 let dataString = getInput("day21.in");
 
-function part1() {
-  let m = new Map(
-    dataString.split(/\n/g).map((line) => {
-      const w = line.split(" ");
-      return w.length === 2
-        ? [w[0].slice(0, -1), () => int(w[1])]
-        : [
-            w[0].slice(0, -1),
-            eval(`() => m.get("${w[1]}")() ${w[2]} m.get("${w[3]}")()`),
-          ];
-    })
-  );
+const lines = dataString
+  .split(/\n/g)
+  .map((line) => line.replace(":", "").split(" "));
 
-  clog(m.get("root")());
+let vals = new Map(
+  lines.filter((w) => w.length === 2).map(([a, v]) => [a, int(v)])
+);
+
+let defs = new Map(
+  lines
+    .filter((w) => w.length === 4)
+    .map(([a, x, op, y]) => [a, [x, op, y] as [string, string, string]])
+);
+
+function value(name: string): number {
+  if (vals.has(name)) return vals.get(name)!;
+  const [X, op, Y] = defs.get(name)!;
+  const [x, y] = [value(X), value(Y)];
+  return op === "+" ? x + y : op === "-" ? x - y : op === "*" ? x * y : x / y;
+}
+
+function part1() {
+  clog(value("root"));
 }
 
 function part2() {
-  let m;
-  m = new Map(
-    dataString.split(/\n/g).map((line) => {
-      const w = line.split(" ");
-      return w.length === 2
-        ? [w[0].slice(0, -1), () => (w[0].slice(0, -1) === "humn" ? "x" : w[1])]
-        : [
-            w[0].slice(0, -1),
-            () =>
-              "(" +
-              m.get(w[1])() +
-              (w[0] === "root:" ? "==" : w[2]) +
-              m.get(w[3])() +
-              ")",
-          ];
-    })
-  );
-  // Part 2: paste into Mathematica with Solve[output, x]
-  clog(m.get("root")());
+  defs.get("root")![1] = "-";
+  vals.set("humn", NaN);
+  // get humn, assuming value(name) = val
+  function getHumn(name: string, val: number) {
+    if (name === "humn") return val;
+    let [left, op, right] = defs.get(name)!;
+    const [x, y] = [value(left), value(right)];
+    const humnLeft = isNaN(x);
+    return getHumn(
+      humnLeft ? left : right,
+      op === "+"
+        ? val - (humnLeft ? y : x)
+        : op === "-"
+        ? humnLeft
+          ? val + y
+          : x - val
+        : op === "/"
+        ? humnLeft
+          ? val * y
+          : x / val
+        : val / (humnLeft ? y : x)
+    );
+  }
+  clog(getHumn("root", 0));
 }
 
 part1();
